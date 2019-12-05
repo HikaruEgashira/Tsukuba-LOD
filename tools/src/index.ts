cb012046b10d25fcd17dcf275949a3cffc9989d4
HikaruEgashira <shighten@gmail.com>
2019-12-05 14:32:53 +0900

    update kdb data

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -1,15 +1,15 @@
 import { downloadKDB, parseKDB, Lecture } from "twinte-parser";
 import { ParsedLecture, ttl } from "./type";
-import * as fs from 'fs'
+import * as fs from "fs";
 
 /**
  * kdb.json
  */
 async function fetchJson(): Promise<Lecture[]> {
-    const csv = await downloadKDB(2019);
-    const json = parseKDB(csv);
-    fs.writeFileSync("kdb.json", JSON.stringify(json));
-    return json;
+  const csv = await downloadKDB(2019);
+  const json = parseKDB(csv);
+  fs.writeFileSync("kdb.json", JSON.stringify(json));
+  return json;
 }
 
 /**
@@ -19,20 +19,20 @@ async function fetchJson(): Promise<Lecture[]> {
  * @param obj
  */
 function divide(obj: Lecture): ParsedLecture[] {
-    const res: ParsedLecture[] = [];
-    for (let i = 0; i < obj.details.length; i++) {
-        const target = {
-            lectureID: obj.lectureID,
-            name: obj.name,
-            instructor: obj.instructor,
-            module: obj.details[i].module,
-            day: obj.details[i].day,
-            period: obj.details[i].period,
-            room: obj.details[i].room
-        };
-        res.push(target)
-    }
-    return res;
+  const res: ParsedLecture[] = [];
+  for (let i = 0; i < obj.details.length; i++) {
+    const target = {
+      lectureCode: obj.lectureCode,
+      name: obj.name,
+      instructor: obj.instructor,
+      module: obj.details[i].module,
+      day: obj.details[i].day,
+      period: obj.details[i].period,
+      room: obj.details[i].room
+    };
+    res.push(target);
+  }
+  return res;
 }
 
 /**
@@ -43,17 +43,25 @@ function divide(obj: Lecture): ParsedLecture[] {
  * @param obj
  */
 function make(lecture: ParsedLecture, id: number): ttl {
-    const prefixs = ["ic:ID型", "ic:名称", "ud:担当者", "ud:期間", "ud:曜日", "ud:時限", "ic:開催場所"];
-    const array = [`_:${id} a ud:授業型`];
-    let i = 0;
-    for (let k in lecture) {
-        array.push(`${prefixs[i]} "${lecture[k]}"`);
-        if (prefixs[i] === "ic:名称") {
-            array.push(`rdfs:label "${lecture[k]}"`);
-        }
-        i++;
+  const prefixs = [
+    "ic:ID型",
+    "ic:名称",
+    "ud:担当者",
+    "ud:期間",
+    "ud:曜日",
+    "ud:時限",
+    "ic:開催場所"
+  ];
+  const array = [`_:${id} a ud:授業型`];
+  let i = 0;
+  for (let k in lecture) {
+    array.push(`${prefixs[i]} "${lecture[k]}"`);
+    if (prefixs[i] === "ic:名称") {
+      array.push(`rdfs:label "${lecture[k]}"`);
     }
-    return array.join(" ;\n\t") + " .";
+    i++;
+  }
+  return array.join(" ;\n\t") + " .";
 }
 
 /**
@@ -63,15 +71,19 @@ function make(lecture: ParsedLecture, id: number): ttl {
  * @param file
  */
 async function parseToLod(jsonObject: Lecture[]) {
-    const parsedArray = await Promise.all(jsonObject
-        .filter(o => o.lectureID.split('')[0] !== "0")
-        .reduce((prev, obj) => {
-            return [...prev, ...divide(obj)];
-        }, []));
-    const lod = await Promise.all(parsedArray.map(function (parsed, i) {
-        return make(parsed, i);
-    }));
-    return lod.join("\n");
+  const parsedArray = await Promise.all(
+    jsonObject
+      .filter(o => o.lectureCode.split("")[0] !== "0")
+      .reduce((prev, obj) => {
+        return [...prev, ...divide(obj)];
+      }, [])
+  );
+  const lod = await Promise.all(
+    parsedArray.map(function(parsed, i) {
+      return make(parsed, i);
+    })
+  );
+  return lod.join("\n");
 }
 
 export { fetchJson, parseToLod };
@@ -79,7 +91,7 @@ export { fetchJson, parseToLod };
 /**
 
 interface Lecture {
-  lectureID: string
+  lectureCode: string
   name: string
   details: {
     module: Module
